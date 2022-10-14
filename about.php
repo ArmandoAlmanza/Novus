020b10380a1de67acb3cf8c113b3e9b8117e3e01
Armando Almanza <armando.s.almanza@gmail.com>
2022-10-13 20:08:21 -0500

    Start the project

---
+++ about.php
@@ -0,0 +1,5 @@
+<?php include_once './src/Components/Header.php';  ?>
+
+<h1>Hello from about</h1>
+
+<?php include_once './src/Components/Footer.php';  ?>
---
+++ src/Components/Footer.php
@@ -0,0 +1,8 @@
+<footer>
+	<h1>I'n the footer</h1>
+</footer>
+
+
+</body>
+
+</html>
