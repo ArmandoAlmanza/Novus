e5213104477c428551ea75dd7082db9e055a7653
Armando Almanza <armando.s.almanza@gmail.com>
2022-10-14 23:34:00 -0500

    header done

--- src/Components/Header.php
+++ src/Components/Header.php
@@ -6,21 +6,52 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Novus</title>
+	<!-- Fonts -->
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,600;1,400&display=swap" rel="stylesheet">
+	<!-- Font awesome -->
+	<script src="https://kit.fontawesome.com/1f29982313.js" crossorigin="anonymous"></script>
+	<!-- Normalize -->
 	<link rel="stylesheet" href="/src/Styles/normalize.css">
+	<!-- Styles -->
 	<link rel="stylesheet" href="/src/Styles/app.css">
 </head>
 
 <body>
 
 	<header class="site__header">
-		<div class="logo__container">
-			<h1 class="logo__text">Novus</h1>
-			<!-- <img src="/src/assets/logoTemporal.jpeg" alt="Logo image" class="logo__img"> -->
+		<div class="header__content">
+			<div class="header__right">
+
+				<div class="logo__container">
+					<a href="/index.php" class="logo__text">Novus</a>
+					<!-- <img src="/src/assets/logoTemporal.jpeg" alt="Logo image" class="logo__img"> -->
+				</div>
+
+				<nav class="navbar">
+					<a href="/about.php">About</a>
+					<a href="#">Discover</a>
+					<a href="#">For man</a>
+					<a href="#">For Woman</a>
+				</nav>
+			</div>
+
+			<div class="header__left">
+
+				<div class="group">
+					<i class="fa-solid fa-magnifying-glass icon"></i>
+					<input placeholder="Search" type="search" class="input" maxlength="14">
+				</div>
+				<nav class="navbar navbar__left">
+					<a href="#">Sign in</a>
+					<a href="#">Bag (0)</a>
+				</nav>
+			</div>
 		</div>
 
-		<nav class="navbar">
-			<a href="/index.php">Home</a>
-			<a href="/about.php">About</a>
-		</nav>
-
+		<div class="hero__content">
+			<h2>Tu estilo no tiene que generar desperdicio</h2>
+			<a href="#" class="btn btn__buy">Shop Now</a>
+		</div>
 	</header>
