4b9f4e1e93087802a0c6cabd5a259bcb73a1be70
Armando Almanza <armando.s.almanza@gmail.com>
2022-10-16 11:47:08 -0500

    add the woman section

--- woman-shop.php
+++ woman-shop.php
@@ -12,7 +12,101 @@
 		<div class="card__info">
 			<h3>Poncho super cool</h3>
 			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione error repellat explicabo deleniti modi fugit, veniam laboriosam, beatae minus inventore similique qui esse. Voluptatibus magnam fuga doloribus, praesentium natus possimus.</p>
-			<span class="price">$39.99</span>
+			<div class="buy__container">
+				<span class="price">$39.99</span>
+				<button class="btn btn__bag">Add to the bag</button>
+			</div>
+		</div>
+	</div>
+	<div class="card">
+		<div class="card__image">
+			<img src="/src/assets/poncho.jpg" alt="poncho">
+		</div>
+		<div class="card__info">
+			<h3>Poncho super cool</h3>
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione error repellat explicabo deleniti modi fugit, veniam laboriosam, beatae minus inventore similique qui esse. Voluptatibus magnam fuga doloribus, praesentium natus possimus.</p>
+			<div class="buy__container">
+				<span class="price">$39.99</span>
+				<button class="btn btn__bag">Add to the bag</button>
+			</div>
+		</div>
+	</div>
+	<div class="card">
+		<div class="card__image">
+			<img src="/src/assets/poncho.jpg" alt="poncho">
+		</div>
+		<div class="card__info">
+			<h3>Poncho super cool</h3>
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione error repellat explicabo deleniti modi fugit, veniam laboriosam, beatae minus inventore similique qui esse. Voluptatibus magnam fuga doloribus, praesentium natus possimus.</p>
+			<div class="buy__container">
+				<span class="price">$39.99</span>
+				<button class="btn btn__bag">Add to the bag</button>
+			</div>
+		</div>
+	</div>
+	<div class="card">
+		<div class="card__image">
+			<img src="/src/assets/poncho.jpg" alt="poncho">
+		</div>
+		<div class="card__info">
+			<h3>Poncho super cool</h3>
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione error repellat explicabo deleniti modi fugit, veniam laboriosam, beatae minus inventore similique qui esse. Voluptatibus magnam fuga doloribus, praesentium natus possimus.</p>
+			<div class="buy__container">
+				<span class="price">$39.99</span>
+				<button class="btn btn__bag">Add to the bag</button>
+			</div>
+		</div>
+	</div>
+	<div class="card">
+		<div class="card__image">
+			<img src="/src/assets/poncho.jpg" alt="poncho">
+		</div>
+		<div class="card__info">
+			<h3>Poncho super cool</h3>
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione error repellat explicabo deleniti modi fugit, veniam laboriosam, beatae minus inventore similique qui esse. Voluptatibus magnam fuga doloribus, praesentium natus possimus.</p>
+			<div class="buy__container">
+				<span class="price">$39.99</span>
+				<button class="btn btn__bag">Add to the bag</button>
+			</div>
+		</div>
+	</div>
+	<div class="card">
+		<div class="card__image">
+			<img src="/src/assets/poncho.jpg" alt="poncho">
+		</div>
+		<div class="card__info">
+			<h3>Poncho super cool</h3>
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione error repellat explicabo deleniti modi fugit, veniam laboriosam, beatae minus inventore similique qui esse. Voluptatibus magnam fuga doloribus, praesentium natus possimus.</p>
+			<div class="buy__container">
+				<span class="price">$39.99</span>
+				<button class="btn btn__bag">Add to the bag</button>
+			</div>
+		</div>
+	</div>
+	<div class="card">
+		<div class="card__image">
+			<img src="/src/assets/poncho.jpg" alt="poncho">
+		</div>
+		<div class="card__info">
+			<h3>Poncho super cool</h3>
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione error repellat explicabo deleniti modi fugit, veniam laboriosam, beatae minus inventore similique qui esse. Voluptatibus magnam fuga doloribus, praesentium natus possimus.</p>
+			<div class="buy__container">
+				<span class="price">$39.99</span>
+				<button class="btn btn__bag">Add to the bag</button>
+			</div>
+		</div>
+	</div>
+	<div class="card">
+		<div class="card__image">
+			<img src="/src/assets/poncho.jpg" alt="poncho">
+		</div>
+		<div class="card__info">
+			<h3>Poncho super cool</h3>
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione error repellat explicabo deleniti modi fugit, veniam laboriosam, beatae minus inventore similique qui esse. Voluptatibus magnam fuga doloribus, praesentium natus possimus.</p>
+			<div class="buy__container">
+				<span class="price">$39.99</span>
+				<button class="btn btn__bag">Add to the bag</button>
+			</div>
 		</div>
 	</div>
 </div>
